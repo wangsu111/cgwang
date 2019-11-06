f16024c8368f008580557abcf23cce3c784c4e63
wangsu111 <707199781@qq.com>
2019-11-06 17:45:11 +0800

    2019

---
+++ maya.js
@@ -0,0 +1,202 @@
+$('.file').click(function(){
+    if(!$(".list1").is(":visible")){
+        $(".list1").show(); 
+       }else{ 
+        $(".list1").hide(); 
+      }
+    $(".list1").siblings().hide();
+    $(".nav1 ul").show();
+})
+
+$('.edit').click(function(){
+    if(!$(".list2").is(":visible")){
+        $(".list2").show(); 
+       }else{ 
+        $(".list2").hide(); 
+      }
+    $(".list2").siblings().hide();
+    $(".nav1 ul").show();
+})
+
+$('.establish').click(function(){
+  if(!$(".list3").is(":visible")){
+      $(".list3").show(); 
+     }else{ 
+      $(".list3").hide(); 
+    }
+  $(".list3").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.select').click(function(){
+  if(!$(".list4").is(":visible")){
+      $(".list4").show(); 
+     }else{ 
+      $(".list4").hide(); 
+    }
+  $(".list4").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.amend').click(function(){
+  if(!$(".list5").is(":visible")){
+      $(".list5").show(); 
+     }else{ 
+      $(".list5").hide(); 
+    }
+  $(".list5").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.display').click(function(){
+  if(!$(".list6").is(":visible")){
+      $(".list6").show(); 
+     }else{ 
+      $(".list6").hide(); 
+    }
+  $(".list6").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.wicket').click(function(){
+  if(!$(".list7").is(":visible")){
+      $(".list7").show(); 
+     }else{ 
+      $(".list7").hide(); 
+    }
+  $(".list7").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.gridding').click(function(){
+  if(!$(".list8").is(":visible")){
+      $(".list8").show(); 
+     }else{ 
+      $(".list8").hide(); 
+    }
+  $(".list8").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.editGrid').click(function(){
+  if(!$(".list9").is(":visible")){
+      $(".list9").show(); 
+     }else{ 
+      $(".list9").hide(); 
+    }
+  $(".list9").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.gridTool').click(function(){
+  if(!$(".list10").is(":visible")){
+      $(".list10").show(); 
+     }else{ 
+      $(".list10").hide(); 
+    }
+  $(".list10").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.gridShow').click(function(){
+  if(!$(".list11").is(":visible")){
+      $(".list11").show(); 
+     }else{ 
+      $(".list11").hide(); 
+    }
+  $(".list11").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.curve').click(function(){
+  if(!$(".list12").is(":visible")){
+      $(".list12").show(); 
+     }else{ 
+      $(".list12").hide(); 
+    }
+  $(".list12").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.hookFace').click(function(){
+  if(!$(".list13").is(":visible")){
+      $(".list13").show(); 
+     }else{ 
+      $(".list13").hide(); 
+    }
+  $(".list13").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.variant').click(function(){
+  if(!$(".list14").is(":visible")){
+      $(".list14").show(); 
+     }else{ 
+      $(".list14").hide(); 
+    }
+  $(".list14").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.UV').click(function(){
+  if(!$(".list15").is(":visible")){
+      $(".list15").show(); 
+     }else{ 
+      $(".list15").hide(); 
+    }
+  $(".list15").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.create').click(function(){
+  if(!$(".list16").is(":visible")){
+      $(".list16").show(); 
+     }else{ 
+      $(".list16").hide(); 
+    }
+  $(".list16").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.cache').click(function(){
+  if(!$(".list17").is(":visible")){
+      $(".list17").show(); 
+     }else{ 
+      $(".list17").hide(); 
+    }
+  $(".list17").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.substance').click(function(){
+  if(!$(".list18").is(":visible")){
+      $(".list18").show(); 
+     }else{ 
+      $(".list18").hide(); 
+    }
+  $(".list18").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+$('.arnold').click(function(){
+  if(!$(".list19").is(":visible")){
+      $(".list19").show(); 
+     }else{ 
+      $(".list19").hide(); 
+    }
+  $(".list19").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+
+$('.help').click(function(){
+  if(!$(".list20").is(":visible")){
+      $(".list20").show(); 
+     }else{ 
+      $(".list20").hide(); 
+    }
+  $(".list20").siblings().hide();
+  $(".nav1 ul").show();
+})
+
+
